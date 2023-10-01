727555861814e3a45c6188845dcdf7bd54b20417
nitin <nitinsachdeva0202@gmail.com>
2023-10-01 23:12:52 +0530

    db file dotenv change

--- db/db.js
+++ db/db.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const uri = process.env.uri
 
 const Main = async () => {
     try {
-        await mongoose.connect(uri, { useNewUrlParser: true, dbName: 'movies' });
+        await mongoose.connect("mongodb+srv://nitinsachdeva0202:Uz9eWvpXOb7dVsmM@cluster0.dlrvzss.mongodb.net/movies?retryWrites=true&w=majority");
         console.log('mongoose is connect');
     } catch (error) {
         console.log('there is one error', error);
